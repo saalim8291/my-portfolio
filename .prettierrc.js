e30d6ab002467d2b1f3041812ad2047cd54f00d6
saalim8291 <saalimbhatkar786@gmail.com>
2026-09-10 13:50:35 +0530

    Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  // Use single quotes instead of double quotes
+  singleQuote: false,
+
+  // Use spaces for indentation (not tabs)
+  useTabs: false,
+
+  // Number of spaces per indentation level
+  tabWidth: 2,
+
+  // Always include parentheses around arrow function parameters, even if there's only one parameter
+  arrowParens: "always",
+
+  // Add trailing commas where valid in ES5 (objects, arrays, etc.)
+  trailingComma: "es5",
+
+  // Keeps the closing bracket on the same line in multiline JSX/TSX/HTML
+  bracketSameLine: true,
+
+  // Keeps code on a single line as much as possible
+  printWidth: 180,
+
+  // 'semi' defaults to true (semicolons are added)
+  // semi: true,
+};
