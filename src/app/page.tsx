a80912863ccd8aeb53a2aabec9fc2f219702c894
saalim8291 <saalimbhatkar786@gmail.com>
2026-09-10 21:50:58 +0530

    Add components to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Header from "@/components/Header/Header";
+import Hero from "@/components/Hero/Hero";
+import Projects from "@/components/Projects/Projects";
+import Skills from "@/components/Skills/Skills";
+import CareerTimeline from "@/components/CareerTimeline/CareerTimeline";
+import Stats from "@/components/Stats/Stats";
+import Articles from "@/components/Articles/Articles";
+import Contact from "@/components/Contact/Contact";
+import Footer from "@/components/Footer/Footer";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}></main>
-    </div>
+    <>
+      <Header />
+      <main>
+        <Hero />
+        <Projects />
+        <Skills />
+        <CareerTimeline />
+        {/* <Stats /> */}
+        {/* <Articles /> */}
+        <Contact />
+      </main>
+      <Footer />
+    </>
   );
 }
